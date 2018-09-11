11cbf5d9a33e9db873b97f7a2171a4ae923e4cca
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2018-09-11 01:08:35 -0300

    Clean up

--- Song.rb
+++ Song.rb
@@ -6,22 +6,10 @@ class Song
     @bpm = params[:bpm]
     @name = params[:name]
     @@song = params[:song]
-    Chord.from_string("4-Am-3")
   end
 
   def perform
     Muse::Song.record(@name, { bpm: @bpm }) do
-      @notes = Muse::NOTES.drop(1)
-      @i = 0
-      @modifiers = {
-        "7" => 10,
-        "maj7" => 11,
-        "sus4" => 5,
-        "add9" => 2,
-        "5aum" => 8,
-        "6" => 9
-      }
-
       @@song.split(" ").map.with_index do |block, i|
         beat = block.split("-")[0]
         chord = Chord.from_string(block)
@@ -31,8 +19,8 @@ class Song
   end
 end
 
-Song.new({
-  bpm: 90,
-  name: "Attention",
-  song: "4-Am-4 4-GM-4 4-Em-3 4-FM-3 8-Am-3"
-}).perform
+# Song.new({
+#   bpm: 90,
+#   name: "Attention",
+#   song: "4-Am-4 4-GM-4 4-Em-3 4-FM-3 8-Am-3"
+# }).perform
